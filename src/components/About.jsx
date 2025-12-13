cf85bea304db9e99dda60e68904245eb491e2b33
Dev-Amna <amnaharaf272@gmail.com>
2025-12-13 10:34:22 +0500

    add stlying on about-me seation

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,20 +1,30 @@
 import React from "react";
 import "../styles/global.css";
+import aboutImg from "../assets/images/about-me.jpg"; // apni image path yahan dal do
+
 function About() {
   return (
     <section id="about" data-aos="fade-up">
-      <h2>About Me</h2>
+      <h2 className="section-title">About Me</h2>
 
-      <p className="about-text">
-        I am a passionate learner of web development. I love creating modern and
-        responsive websites. Currently learning React and improving my
-        JavaScript skills.
-      </p>
+      <div className="about-container">
+        <div className="about-image" data-aos="zoom-in">
+          <img src={aboutImg} alt="About Me" />
+        </div>
 
-      <div className="about-info">
-        <span>Beginner</span>
-        <span>React Enthusiast</span>
-        <span>Based in Pakistan</span>
+        <div className="about-content" data-aos="fade-left">
+          <p className="about-text">
+            I am a passionate learner of web development. I love creating modern and
+            responsive websites. Currently learning React and improving my
+            JavaScript skills.
+          </p>
+
+          <div className="about-info">
+            <span>Beginner</span>
+            <span>React Enthusiast</span>
+            <span>Based in Pakistan</span>
+          </div>
+        </div>
       </div>
     </section>
   );

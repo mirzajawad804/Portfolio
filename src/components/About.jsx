3d67840ae4d34b734097530d227fbdcb4f9ce8b1
Dev-Amna <amnaharaf272@gmail.com>
2025-12-27 16:23:40 +0500

    Change images

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../styles/global.css";
-import aboutImg from "../assets/images/about-me.jpg"; 
+import aboutImg from "../assets/images/about-me.webp"; 
 
 function About() {
   return (
